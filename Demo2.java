fb7a8d9457ea704205d4e8ada2a24667f585b1f8
sridharr1 <sridharalkunta@gmail.com>
2025-01-02 14:25:49 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+class Demo2{
+    public static void main (String []args) {
+        int num1, num2;
+        num1 = 10;
+        num2 = 15;
+        if(num1>num2){
+            System.out.println("num1 is geater");
+        }
+        else{
+        System.out.println("num2 is greater");
+        }
+    }
+}
