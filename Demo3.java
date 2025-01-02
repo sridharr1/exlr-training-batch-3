39cb10c7effba5fbfb06fed6f0a709da7d872358
sridharr1 <sridharalkunta@gmail.com>
2025-01-02 14:31:44 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+class Demo3{
+    public static void main (String []args) {
+        int num1, num2, num3;
+        num1 = 10;
+        num2 = 15;
+        num3 = 20;
+        if(num1>num2 & num1>num3){
+            System.out.println("num1 is geater");
+        }
+        else if(num2>num1 & num2>num3){
+            System.out.println("num2 is greater");
+        }
+        else {
+            System.out.println("num3 is greater");
+    }
+}
+}
