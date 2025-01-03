848ba6cf49aa87c6805a5dbd1b798c61cbf09c56
sridharr1 <sridharalkunta@gmail.com>
2025-01-03 11:28:14 +0530

    Add files via upload

---
+++ UserCredentials2.java
@@ -0,0 +1,10 @@
+public class UserCredentials2{
+    public static void main(String[] args){
+        String username =  "Sridhar";
+        String password =  "Sridhar@123";
+        while((username == "Sridhar") && (password == "Sridhar@123")){
+            System.out.println("Hello Sridhar welcome to Mallareddy University");
+            break;
+        }
+    }
+}
