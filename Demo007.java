e41e5748aac8fa942d96b57cd8a1faadbf35374a
sridharr1 <sridharalkunta@gmail.com>
2025-01-03 11:32:44 +0530

    Add files via upload

---
+++ Demo007.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Demo007 {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        Long phoneNo;
+        System.out.println("Please enter your name, age, phoneNo");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        age = scanner.nextInt();
+        phoneNo = scanner.nextLong();
+        System.out.println("Hello"+" "+ name +" "+ "ur age is"+" "+ age +" "+ "ur phoneNo is"+" "+ phoneNo);
+
+    }
+}
