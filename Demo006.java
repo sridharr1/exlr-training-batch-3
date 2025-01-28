28303545c66bb29372174ec48995157c8a4a1f6b
sridharr1 <sridharalkunta@gmail.com>
2025-01-28 12:39:25 +0530

    User name and Password

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String name = "Rajkumar";
+        String pwd = "Raj@87";
+        while ((name == "Rajkumar") && (pwd == "Raj@87")){
+            System.out.println("Hello Rajkumar welcome to mallareddy");
+                break;
+            }
+         }
+    }
