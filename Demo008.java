252dd7a98a30790f92c0e1e9066b96a30f1c7426
sridharr1 <sridharalkunta@gmail.com>
2025-01-28 12:41:23 +0530

    Uni Login Page

--- Demo008.java
+++ Demo008.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Demo8 {
+public class Demo008 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String username,pass;
