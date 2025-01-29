f583065a410d826e5e83b9885d7a9546a461587e
sridharr1 <sridharalkunta@gmail.com>
2025-01-29 12:39:28 +0530

    Hw-palindrome(more than 3 digits)

--- Demo023.java
+++ Demo023.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Demo23 {
+public class Demo023 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
